e6abdd37b706a53a0e6ee91af871c64e7d2d3c22
Prathana Dhungel <pd29@bu.edu>
2021-09-30 16:03:15 -0400

    implemented read_csv

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,10 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    data = open(csv_file_path, "r")
+    lines = data.readlines()
+    output = []
+    for line in lines:
+        output.append([line])
+    
+    return output
